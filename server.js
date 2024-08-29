3e80e421c38f018a1671411084b0a37ffd8e1298
EhSlashEh <anthonyalanb@hotmail.com>
2024-08-29 13:08:32 -0300

    sync

--- server.js
+++ server.js
@@ -14,13 +14,42 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Store hash in your db
+      console.log(`Hashed password: ${hash}`);
+  
+      // Compare the plaintext password with the hashed password
+      bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Password match: ${res}`); // res == true or false
+        }
+      });
+    }
+  });
+  
+  bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Hashed 'passw0rd!': ${hash}`);
+      bcrypt.compare('passw0rd!', hash, (err, res) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(res); // true
+        }
+      });
+    }
+  });
 
 //END_ASYNC
 
 //START_SYNC
-
-
 
 //END_SYNC
 
